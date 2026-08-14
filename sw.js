37ef4028fff4261e63fd3b15e17f90bae181f798
polinasyon <lirikseli@gmail.com>
2026-08-14 18:30:41 +0300

    Update CACHE_VERSION comment in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 11; // Versiyonu güncelledik
+const CACHE_VERSION = 11; //
 const CACHE_NAME = `polinasyon-static-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `polinasyon-dynamic-v${CACHE_VERSION}`;
 
