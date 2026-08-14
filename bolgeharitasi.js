f180b5a7e1685457566699708393291091bbfbf4
polinasyon <lirikseli@gmail.com>
2026-08-14 14:53:02 +0300

    Create bolgeharitasi.js

---
+++ bolgeharitasi.js
@@ -0,0 +1,34 @@
+window.BOLGE_HARITASI = {
+  "iç anadolu": [
+    "ankara", "konya", "kayseri", "eskişehir", "sivas", "kırıkkale", "aksaray",
+    "karaman", "kırşehir", "niğde", "nevşehir", "yozgat", "çankırı",
+    "kızılcahamam", "nallıhan", "beypazarı", "emirdağ", "seydişehir", "bozkır", "sungurlu"
+  ],
+  "ege": [
+    "izmir", "manisa", "aydın", "denizli", "kütahya", "afyon", "afyonkarahisar", "uşak", "muğla",
+    "simav", "acıpayam", "tavas", "dinar", "sandıklı", "fethiye", "marmaris"
+  ],
+  "akdeniz": [
+    "antalya", "adana", "mersin", "burdur", "hatay", "osmaniye", "ısparta", "isparta", "kahramanmaraş",
+    "elmalı", "elmali", "kaş"
+  ],
+  "karadeniz": [
+    "trabzon", "rize", "artvin", "ordu", "giresun", "samsun", "zonguldak", "sinop", "tokat",
+    "çorum", "amasya", "kastamonu", "karabük", "bartın", "bolu", "düzce",
+    "gümüşhane", "bayburt",
+    "ilgaz", "tosya", "merzifon", "vezirköprü", "şebinkarahisar", "kelkit", "yusufeli", "iskilip"
+  ],
+  "doğu anadolu": [
+    "erzurum", "erzincan", "kars", "ardahan", "ağrı", "ığdır", "van", "bingöl", "muş",
+    "bitlis", "tunceli", "elazığ", "malatya", "hakkari"
+  ],
+  "marmara": [
+    "istanbul", "kocaeli", "sakarya", "yalova", "bursa", "balıkesir", "çanakkale",
+    "tekirdağ", "edirne", "kırklareli", "bilecik",
+    "edremit", "ayvacık"
+  ],
+  "güneydoğu": [
+    "gaziantep", "şanlıurfa", "diyarbakır", "mardin", "batman", "siirt",
+    "adıyaman", "kilis", "şırnak"
+  ]
+};
